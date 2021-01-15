bde6926ca77f60764d401a82ab04f0b5683ac0b2
Yiğit Taha Öztürk <77507219+yigitozturk@users.noreply.github.com>
2021-01-16 00:36:50 +0300

    Update daire alani ve cevre hesaplama.py

--- daire alani ve cevre hesaplama.py
+++ daire alani ve cevre hesaplama.py
@@ -1,14 +1,49 @@
-# Daire Alanı Hesaplayıcı
-pi = float(input("Pi? "))
-yaricap = float(input("Yarıçap? "))
-yaricapkare = yaricap**2
-dairealani = yaricapkare*pi
-print("")
-print("")
-print("Daire alanı {}".format(dairealani))
+def hosgeldin():
+    print("""
+Daire hesaplayıcısına hoşgeldin!
+""")
 
-# Daire Çevresi Hesaplayıcı
-dairecevresi = pi*2*yaricap
-print("")
-print("")
-print("Daire Çevresi {}".format(dairecevresi))
+hosgeldin()
+
+def hesaplama():
+    operation = input("""
+Yapmak istediğin işlemi komut satırına yaz;
+Daire alanı için ALAN
+Daire çevresi için ÇEVRE
+
+""")
+
+    pi = float(input("Pi sayısını yazınız: "))
+    yaricap = float(input("Yarıçapı yazınız: "))
+    capkare = (yaricap**2)
+
+    if operation == 'ALAN':
+        print("{} * {}".format(pi,capkare))
+        print(pi*capkare)
+        
+    elif operation == 'ÇEVRE':
+        print("2 * {} * {}".format(pi,yaricap))
+        print(2*pi*yaricap)
+    else:
+        print("")
+        print("Yanlış bir komut girdiniz lütfen doğru yazınız.")
+        
+    tekrar()
+
+def tekrar():
+    tekraret = input(""" 
+Tekrar hesaplama yapmak istiyormusun?
+Lütfen yapmak istiyor isen E
+Yapmamak istiyor isen H yaz.
+
+""")
+
+    if tekraret.upper() == 'E':
+        hesaplama()
+    elif tekraret.upper() == 'H':
+        print("Daha sonra görüşürüz...")
+    else:
+        tekrar()
+        
+        
+hesaplama()
