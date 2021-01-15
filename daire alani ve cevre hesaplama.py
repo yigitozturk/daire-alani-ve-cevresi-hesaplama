cc7d6ee24df3d4fe4c032c6018a9364bd38624a3
Yiğit Taha Öztürk <77507219+yigitozturk@users.noreply.github.com>
2021-01-15 21:32:28 +0300

    Create daire alani ve cevre hesaplama.py

---
+++ daire alani ve cevre hesaplama.py
@@ -0,0 +1,14 @@
+# Daire Alanı Hesaplayıcı
+pi = float(input("Pi? "))
+yaricap = float(input("Yarıçap? "))
+yaricapkare = yaricap**2
+dairealani = yaricapkare*pi
+print("")
+print("")
+print("Daire alanı {}".format(dairealani))
+
+#Daire Çevresi Hesaplayıcı
+dairecevresi = pi*2*yaricap
+print("")
+print("")
+print("Daire Çevresi {}".format(dairecevresi))
